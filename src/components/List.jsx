6c6008f0668b1b43d76c0096a4a514da90fbfd6c
ggambopce <ggambopce@gmail.com>
2024-11-11 13:36:51 +0900

    헤더,에디터,리스트UI 구현

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,5 +1,11 @@
+import "./List.css";
+
 const List = () => {
-    return <div>List</div>
+    return <div className="List">
+        <h4>Todo List 🖌️</h4>
+        <input placeholder="검색어를 입력하세요" />
+
+    </div>
 }
 
 export default List;
