24763755d56f17dc626aa8bc7790b010a0365220
ggambopce <ggambopce@gmail.com>
2024-11-11 14:03:23 +0900

    UI 구현 완료

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,9 +1,15 @@
 import "./List.css";
+import TodoItem from "./TodoItem";
 
 const List = () => {
     return <div className="List">
         <h4>Todo List 🖌️</h4>
         <input placeholder="검색어를 입력하세요" />
+        <div className="todos_wrapper">
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+        </div>
 
     </div>
 }
