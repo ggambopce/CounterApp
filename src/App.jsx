ebad97656f6dce5a13af9c00c602ced02af2d594
ggambopce <ggambopce@gmail.com>
2024-11-11 14:57:18 +0900

    mockData준비

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import './App.css'
+import { useState } from 'react'
 import Header from './components/Header'
 import Editor from './components/Editor'
 import List from './components/List'
-function App() {
 
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "농구하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "청소하기",
+    date: new Date().getTime(),
+  },
+]
+
+
+function App() {
+  
+  
+  const [todos, setTodos] = useState(mockData);
+  
   return (
     <div className="App">
      <Header />
