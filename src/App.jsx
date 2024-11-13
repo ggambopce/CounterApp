89486b579c777fb5497bb6443c7b61ddfceafe45
ggambopce <ggambopce@gmail.com>
2024-11-13 13:05:20 +0900

    context 사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useState, useRef, useReducer } from 'react'
+import {  useRef, useReducer, useCallback, createContext } from 'react'
 import Header from './components/Header'
 import Editor from './components/Editor'
 import List from './components/List'
@@ -43,11 +43,13 @@ function reducer(state, action) {
     }
 }
 
+export const TodoContext = createContext();
+
 function App() { 
   const idRef = useRef(3);
   const [todos, dispatch] = useReducer(reducer, mockData);
   
-  const onCreate = (content) =>{
+  const onCreate = useCallback((content) =>{
     dispatch({
       type: "CREATE",
       data: {
@@ -57,28 +59,31 @@ function App() {
         date: new Date().getTime(),
       },
     });
-  };
+  }, [])
 
-  const onUpdate = (targetId) => {
+  const onUpdate = useCallback((targetId) => {
     dispatch({
       type: "UPDATE",
       targetId: targetId,
     })
-  };
+  },[]);
 
-  const onDelete = (targetId)=>{
+  const onDelete = useCallback((targetId)=>{
     dispatch({
       type: "DELETE",
       targetId: targetId,
     });
-  }
+  }, []); 
 
   return (
     <div className="App">
      <Header />
-     <Editor onCreate={onCreate} />
-     <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
+     <TodoContext.Provider value={{todos, onCreate, onUpdate, onDelete}}>
+     <Editor />
+     <List />
 
+     </TodoContext.Provider>
+     
     </div>
   )
 }
