2fc44dab58399d56fa593e0c894136ee665c7a11
ggambopce <ggambopce@gmail.com>
2024-11-13 11:51:43 +0900

    memo, useCallback 렌더링 최적화

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import "./Header.css";
+import { memo } from "react";
 
 const Header = () => {
     return <div className="Header">
@@ -7,4 +8,4 @@ const Header = () => {
 </div>
 }
 
-export default Header;
+export default memo(Header);
